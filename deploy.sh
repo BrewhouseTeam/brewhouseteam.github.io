817d11d83741607659f4436d085ea44a85ea38a7
Charles Bergeron <chuck.bergeron@gmail.com>
2014-03-07 10:46:02 -0800

    deploy script updates

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
-git checkout source
-git add --all
-git ci -m "auto deploy"
+# git checkout source
+# git add --all
+# git ci -m "auto deploy"
 
+# bundle exec jekyll build
+
+# git branch -D master
+# git checkout -b master
+
+# git filter-branch --subdirectory-filter _site/ -f
+# git checkout source
+
+# git push --all origin
+
+git co source
 bundle exec jekyll build
 
-git branch -D master
-git checkout -b master
-
-git filter-branch --subdirectory-filter _site/ -f
-git checkout source
-
-git push --all origin
+rm -rf /tmp/_site
+cp -r _site /tmp/_site
+git checkout master
+cp -r /tmp/_site/* .
+git add .
+git commit -a -m "auto deploy :boom:"
+git push origin master
+git co source
