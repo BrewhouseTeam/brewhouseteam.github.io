c76b3f60afd0cb04b39b951ab61c336407b79812
Charles Bergeron <chuck.bergeron@gmail.com>
2014-03-06 18:50:54 -0800

    this is where we'll do our dev work on this site

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 bundle exec jekyll build
-git add --all
-git ci -m "auto deploy"
-git push origin master
+
+git branch -D master
+git checkout -b master
+git filter-branch --subdirectory-filter _site/ -f
+git checkout source
+git push --all origin
+
+# git ci -m "auto deploy"
+# git push origin master
