137331c8b5db539730bc85412bf719af02431d1b
Godfrey Chan <godfreykfc@gmail.com>
2014-04-01 12:37:52 -0700

    WIP blog

--- _assets/javascripts/brewhouse.js
+++ _assets/javascripts/brewhouse.js
@@ -1,9 +1,24 @@
-(function() {
+jQuery(function($) {
 
-  var $document = $(document),
-      $navbar   = $('#navbar-main');
+  var $document   = $(document),
+      $navbar     = $('.navbar-main'),
+      $blogBanner = $('#banner-blog'),
+      $blogImage  = $('img', $blogBanner),
+      lastScroll, now, delta;
+
+  if ($blogBanner.length && $blogImage.length){
+    delta = $blogImage.height() - $blogBanner.height();
+    console.log(delta);
+  }
 
   $document.scroll(function() {
+    now = new Date().valueOf();
+
+    if(lastScroll && now - lastScroll < 25){
+      return;
+    }else{
+      lastScroll = now;
+    }
 
     if ($document.scrollTop() >= 5) {
       $navbar.addClass('sticky');
@@ -11,6 +26,9 @@
       $navbar.removeClass('sticky');
     }
 
+    if ($blogBanner.length && $blogImage.length && delta > 0){
+      $blogImage.css({marginTop: Math.max(-delta, -$document.scrollTop() / 5)});
+    }
   });
 
   var $rootNode = $('html, body');
@@ -27,5 +45,4 @@
 
     return false;
   });
-
-})();
+});
