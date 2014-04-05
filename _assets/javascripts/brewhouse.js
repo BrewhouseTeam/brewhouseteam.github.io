3a44e2384011fc1662d908bcd7faf448a8f5688a
Charles Bergeron <chuck.bergeron@gmail.com>
2014-04-04 17:32:26 -0700

    ajax form mailchimp stuff

--- _assets/javascripts/brewhouse.js
+++ _assets/javascripts/brewhouse.js
@@ -49,4 +49,46 @@ jQuery(function($) {
 
     return false;
   });
+
+
+  // I only have one form on the page but you can be more specific if need be.
+  var $form = $('#mc_embed_signup form');
+
+  if ( $form.length > 0 ) {
+    $('form input[type="submit"]').bind('click', function ( event ) {
+      if ( event ) {
+        event.preventDefault();
+      }
+
+      // validate_input() is a validation function I wrote,
+      // you'll have to substitute this with your own.
+      // if ( validate_input($form) ) {
+        register($form);
+      // }
+    });
+  }
+
 });
+
+function register($form) {
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache       : false,
+    dataType    : 'json',
+    contentType: "application/json; charset=utf-8",
+    error       : function(err) {
+      alert("Could not connect to the registration server. Please try again later.");
+    },
+    success     : function(data) {
+      if (data.result != "success") {
+        alert(data.msg);
+        // Something went wrong, do something to notify the user. maybe alert(data.msg);
+      } else {
+        alert('all good!');
+        // It worked, carry on...
+      }
+    }
+  });
+}
