7e8ccb5f55be562bce8875ac5fcc83d981ace5f2
Charles Bergeron <chuck.bergeron@gmail.com>
2014-12-30 12:32:15 -0800

    remove CDN calls for foundation icons and Lato font, simplify blog banner and add contact info to it

--- _assets/javascripts/brewhouse.js
+++ _assets/javascripts/brewhouse.js
@@ -1,41 +1,19 @@
 jQuery(function($) {
 
-  var $document   = $(document),
-      $navbar     = $('.navbar-main'),
-      $blogBanner = $('#banner-blog'),
-      $blogImage  = $('img', $blogBanner),
-      lastScroll, now, _delta;
-
-  // Parallax banner
-  // if ($blogBanner.length && $blogImage.length){
-  //   _delta = $blogImage.height() - $blogBanner.height();
-  // }
+  var $document = $(document),
+    $navbar     = $('.navbar-main'),
+    $rootNode   = $('html, body');
 
   $document.scroll(function() {
-    // Parallax banner
-    // now = new Date().valueOf();
-
-    // if (lastScroll && now - lastScroll < 50){
-    //   return;
-    // } else {
-    //   lastScroll = now;
-    // }
-
-    // if ($blogBanner.length && $blogImage.length && _delta > 0){
-    //   var newTop = Math.max(-_delta, -$document.scrollTop() / 4);
-    //   console.log(newTop);
-    //   $blogImage.css({marginTop: newTop});
-    // }
-    // END Parallax banner
 
     if ($document.scrollTop() >= 5) {
       $navbar.addClass('sticky');
     } else {
       $navbar.removeClass('sticky');
     }
+
   });
 
-  var $rootNode = $('html, body');
   $('a.internal').on('click', function(){
     var href = $.attr(this, 'href');
 
