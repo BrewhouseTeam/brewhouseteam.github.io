5898fc6044b6e8a5ad441c58514a8c9dd4133c56
Charles Bergeron <chuck.bergeron@gmail.com>
2015-03-06 13:54:49 -0800

    fix up an offcanvas menu solution for mobile Brewhouse site, better than having no menu on there, which is what we had before :P

--- _assets/javascripts/brewhouse.js
+++ _assets/javascripts/brewhouse.js
@@ -3,20 +3,43 @@ jQuery(function($) {
   var $document = $(document),
     $navbar     = $('.navbar-main'),
     $rootNode   = $('html, body'),
-    $footer     = $('footer');
+    $footer     = $('footer'),
+    $offcanvasToggle = $('.js-navbar-toggle-offcanvas');
 
+
+  // All offcanvas related code
+  var toggleOffcanvasMenu = function(){
+    var $offcanvasMenu = $('.offcanvas-menu'), moveTo;
+    $offcanvasMenu.css('height', $(window).height());
+
+    if (parseInt($('.offcanvas-menu').css('top')) < 0) {
+      moveTo = 0;
+    } else {
+      moveTo = -($(window).height());
+    };
+
+    $offcanvasMenu.velocity({ top: moveTo }, { duration: 100, easing: 'easeInCirc' });
+  };
+
+  $offcanvasToggle.on('click', toggleOffcanvasMenu);
+  $('.offcanvas-menu').find('a.internal').on('click', toggleOffcanvasMenu);
+
+
+
+  // Set Copyright Year
   $footer.find('.copyright-year').html('2015');
 
-  $document.scroll(function() {
 
+  // Set Sticky on scroll (should also be on page load)
+  $document.scroll(function() {
     if ($document.scrollTop() >= 5) {
       $navbar.addClass('sticky');
     } else {
       $navbar.removeClass('sticky');
     }
-
   });
 
+  // Have menu items which point to content on this page scroll the body
   $('a.internal').on('click', function(){
     var href = $.attr(this, 'href');
 
@@ -26,11 +49,12 @@ jQuery(function($) {
 
     $rootNode.animate({
       scrollTop: $(href).offset().top
-    }, 1500, 'easeInOutExpo', addHrefToHash);
+    }, 2500, 'easeInOutExpo', addHrefToHash);
 
     return false;
   });
 
+  // A hack for pushing people down to the content, I think this can be removed now but needs testing
   if(window.location.search.indexOf("brewPubStart") > -1){
     var $elem = $('h1#brew-pub-start');
 
@@ -40,6 +64,7 @@ jQuery(function($) {
 
   }
 
+  // Refactor this video stuff into one func
   if ( $("video.team-video").length ){
     if($(window).width() > 750) {
       $("video.team-video")[0].preload = "auto";
@@ -51,6 +76,7 @@ jQuery(function($) {
 });
 
 $(window).resize(function() {
+  // Refactor this video stuff into one func
   if ( $("video.team-video").length ){
     if($(window).width() > 750) {
       $("video.team-video")[0].preload = "auto";
