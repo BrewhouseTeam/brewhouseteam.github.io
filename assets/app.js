eb331e894e6a0ee5dc82456535c54d8faf80c005
Kalvir Sandhu <kalv@kalv.co.uk>
2016-04-01 07:09:53 -0700

    auto deploy :boom:

--- assets/app.js
+++ assets/app.js
@@ -13269,6 +13269,26 @@ jQuery(function($) {
   });
 
 });
+$.fn.scrollEnd = function(callback, timeout) {
+  $(this).scroll(function(){
+    var $this = $(this);
+    if ($this.data('scrollTimeout')) {
+      clearTimeout($this.data('scrollTimeout'));
+    }
+    $this.data('scrollTimeout', setTimeout(callback,timeout));
+  });
+};
+
+//jQuery(function($) {
+  $(window).scroll(function(){
+    $('.party').addClass('scrolling-party')
+  })
+
+  $(window).scrollEnd(function(){
+    $('.party').removeClass('scrolling-party')
+  }, 100);
+//});
+
 
 
 
